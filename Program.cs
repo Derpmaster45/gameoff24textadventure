b4f51270bac85291fe0bab582d95b775f6ef0694
Derpmaster45 <jmsteltz04@gmail.com>
2024-11-02 13:27:39 -0500

    started story

--- Program.cs
+++ Program.cs
@@ -41,7 +41,35 @@
                 {
                     case "1":
                     case"new game":
-                        System.Console.WriteLine("New game started\n");
+                        //System.Console.WriteLine("New game started\n");
+                        Console.WriteLine("The year is 1849, You have set out to find information on the disappearance of a civilization known as Shattered Harmony Refuge.\n The first thing that you are tasked to do is find a potential location of this civilization. Luckily, you have texts that may give you location.\n You may also look at maps to deduce the location, without the books, the location may most likely be inacurate." );
+                        // switch to prompt player for first course of action
+                        string PlayerInitChoice="";
+                        while(PlayerInitChoice=="")
+                        {
+                            Console.WriteLine("Would you like to: 1) Read historical books \n 2) Look at maps");
+                            PlayerInitChoice=Console.ReadLine();
+                            switch(PlayerInitChoice.ToLower())
+                            {
+                                // read books
+                                case "1":
+                                    System.Console.WriteLine();
+                                break;
+                                // look at maps
+                                case "2":
+                                    System.Console.WriteLine();
+                                break;
+                                case "q":
+                                    QuitGame();
+                                break;
+                                default:
+                                    ResetAndClear("Please choose from \n 1) Read through historical texts \n 2) Look at maps\n",PlayerInitChoice);
+                                break;
+                            }
+                        }
+                        
+
+
                     break;
                     case "2":
                     case "quit game":
