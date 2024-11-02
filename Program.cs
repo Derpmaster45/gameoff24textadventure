dee3a9f027cf47bf23103da20341c76b4b9cc22e
Derpmaster45 <jmsteltz04@gmail.com>
2024-11-02 10:48:05 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Gameoff24
+{
+    class Gameoff24
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Game Off 2024 Enrry\n 1) New game\n 2) Quit Game\n");
+            string MainMenuOption="";
+
+
+        }
+    }
+}
